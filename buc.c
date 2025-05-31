55a180b3b35247a8ee2c12e94c19befdba09550b
Nakidai <nakidai@disroot.org>
2025-05-31 16:00:41 +0300

    Improvements
    - Remove unused `all` variable
    - Fix port parsing

--- buc.c
+++ buc.c
@@ -3,14 +3,14 @@
 #include <netinet/in.h>
 #include <poll.h>
 
-char buf[512]; progress, all;
+char buf[512]; progress;
 atoi(), err(), exit(), read(), write();
 
 #define	readbuf(sock)	((((progress = read((sock), buf, sizeof(buf))) == -1) && err(1, "read()") || !progress && exit(0)), progress)
 #define	sendbuf(sock, size)	(write(sock, buf, size) == -1 && err(1, "write()"))
 
 main(argc, argv) char **argv; {
-	struct sockaddr_in addr = {AF_INET, htons(argc > 2 ? atoi(argv[1]) : 8604), inet_addr(argc > 1 ? argv[1] : "127.0.0.1")};
+	struct sockaddr_in addr = {AF_INET, htons(argc > 2 ? atoi(argv[2]) : 8604), inet_addr(argc > 1 ? argv[1] : "127.0.0.1")};
 	struct pollfd fds[2] = {{0, POLLIN}, {0, POLLIN}};
 	for (	(fds[1].fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 && err(1, "socket()"),
 	     	connect(fds[1].fd, (void *)&addr, sizeof(addr)) == -1 && err(1, "connect()");
